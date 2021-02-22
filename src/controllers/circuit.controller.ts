acaf73928a392d3ebd2dbd346216b63b99e9bef9
Jhonattan Garcia <jhonattanrgc21@gmail.com>
2021-02-22 17:13:44 -0400

    Agregue una liea que faltaba en el controlador de Circuit

--- src/controllers/circuit.controller.ts
+++ src/controllers/circuit.controller.ts
@@ -24,7 +24,7 @@ export default class CircuitController {
 			return res.status(404).json({
 				message: 'Error, no existen circuitos registrados.',
 			});
-	
+
 	};
 
 	static listForm = (req: Request, res: Response) => {
