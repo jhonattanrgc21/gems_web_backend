afc42f4ed32ff85ca4056c6e4b84f004da597002
Jhonattan Garcia <jhonattanrgc21@gmail.com>
2021-05-06 20:28:12 -0400

    cambie la clave para enviar correos en mailer.ts

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -6,7 +6,7 @@ export const transporter = nodemailer.createTransport({
 	secure: true, // true for 465, false for other ports
 	auth: {
 		user: 'jhonattanrgc21@gmail.com', // generated ethereal user
-		pass: 'npesyqfkpebozowg', // generated ethereal password
+		pass: 'supcvrtmzrxzmuck', // generated ethereal password
 	},
 });
 
